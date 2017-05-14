4e16d230592b9e4ea28fce34e930a60ff846b05b
chnbohwr <chnbohwr@gmail.com>
2017-05-14 19:57:36 +0800

    add event listener and readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Simple React Start Kit 2017
+# react_challenge_Key_Sequence_Detection
 
-這個環境包可以讓你快速體驗 ReactJS 開發環境的便利，我們並沒有在裏面放入太多套件(例如 redux, karma)，希望可以讓你用最簡單的環境開始學習 React
+可以讓網頁偵測使用者輸入的字，當字串符合密碼以後解鎖按鈕
+
+## [DEMO](https://reactmaker.github.io/react_challenge_Key_Sequence_Detection/#/)
 
 ## 特色
 
@@ -19,8 +21,8 @@
 先確定好安裝了 node 4.5 以上的版本，接著就可以輸入以下指令
 
 ```bash
-$ git clone https://github.com/ReactMaker/simple_react_start_kit_2017
-$ cd simple_react_start_kit_2017
+$ git clone https://github.com/ReactMaker/react_challenge_Key_Sequence_Detection
+$ cd react_challenge_Key_Sequence_Detection
 $ npm install                   # Install project dependencies
 $ npm start                     # Compile and launch
 ```
@@ -28,6 +30,7 @@ $ npm start                     # Compile and launch
 如果一切成功，就會看到以下畫面，並且瀏覽器會打開的範例頁面
 
 ![Imgur](http://i.imgur.com/14aROBn.png)
+![Imgur](http://i.imgur.com/WPv46uy.gifv)
 
 開發當中最常用到的是 `npm start` 指令，我們還有一些其他的指令要介紹給你知道
 
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,10 +3,45 @@ import React, { Component } from 'react';
 import './Home.less';
 
 export default class Home extends Component {
+
+  state = {
+    pressed: [],
+    secret: ['r', 'e', 'a', 'c', 't'],
+    buttonDisabled: true,
+  }
+
+  componentDidMount() {
+    window.addEventListener('keyup', this.onKeyPress);
+  }
+
+  onKeyPress = (e) => {
+    const key = e.key;
+    if (key === this.state.secret[this.state.pressed.length]) {
+      const newPressed = this.state.pressed;
+      newPressed.push(key);
+      if (newPressed.join('') === this.state.secret.join('')) {
+        this.setState({ pressed: newPressed, buttonDisabled: false });
+      } else {
+        this.setState({ pressed: newPressed });
+      }
+    } else {
+      this.setState({ pressed: [], buttonDisabled: true });
+    }
+
+  }
+
   render() {
     return (
       <div id="pageHome">
-        this is home page
+        <button disabled={this.state.buttonDisabled} onClick={() => alert('恭喜解鎖成功')}>Click me</button>
+        <div className="notice">請用鍵盤輸入以下英文就可以解鎖按鈕了</div>
+        <div className="example">
+          {this.state.secret.map(code => (<div key={Math.random()}>{code}</div>))}
+        </div>
+        <div className="notice">目前輸入</div>
+        <div className="example">
+          {this.state.pressed.map(code => (<div key={Math.random()}>{code}</div>))}
+        </div>
       </div>
     );
   }
